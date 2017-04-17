eb93ba670543d5e9bfc552ee6a1c6996e6788c0e
Abdur Rauof Thameem <thameem@vizvastudio.com>
2017-04-18 00:38:44 +0530

    First version of dynamodb command line utility

---
+++ bin/dynamodb-util.js
@@ -0,0 +1,81 @@
+#!/usr/bin/env node
+
+const argv = require('yargs').argv;
+const AWS = require('aws-sdk');
+const dynamoDBHelper = require('../lib/dynamodb-helper.js')
+const log = require('../lib/log-helper.js')
+const backupHelper = require('../lib/backup-helper.js')
+
+var args = {
+    config: '',
+    command: ['listtables', 'listrecords', 'describe', 'backup'],
+}
+
+try {
+    log.i(argv);
+    if (argv.config && argv.config.endsWith('.json')) {
+        var config = require(argv.config.replace('\\', '/'));
+        log.i(config);
+        if (argv.command && args.command.indexOf(argv.command) >= 0) {
+            if (config.source.aws)
+                AWS.config.update(config.source.aws);
+            var command = argv.command;
+            log.i(command);
+            switch (argv.command) {
+                case 'listtables':
+                    dynamoDBHelper.listTables(AWS, (err, data) => {
+                        if (err)
+                            log.e(err);
+                        log.i(data);
+                    })
+                    break;
+                case 'describe':
+                    var param = { TableName: config.source.TableName }
+                    dynamoDBHelper.describeTable(AWS, param, (err, data) => {
+                        if (err)
+                            log.e(err);
+                        log.i(JSON.stringify(data));
+                    })
+                    break;
+                case 'backup':
+                    if (config.target) {
+                        if (config.target.type) {
+                            switch (config.target.type) {
+                                case 'json':
+                                    backupHelper.backupDynamoDBToJSON(config.source, config.target, (err, result) => {
+                                        log.i(err, result);
+                                    })
+                                    break;
+                                case 'dynamoDB':
+                                    backupHelper.backupDynamoDBToDynamoDB(config.source, config.target, (err, result) => {
+                                        log.i(err, result);
+                                    })
+                                    break;
+                                default:
+                                    break;
+                            }
+                        }
+                        else
+                            log.e('Target type not defined in the config');
+                    }
+                    else
+                        log.e('Target not defined in the config');
+                    break;
+                default:
+                    break;
+            }
+
+        }
+        else {
+            log.i(args.command.indexOf(argv.command));
+            log.e('Invalid or no command passed')
+        }
+    }
+    else {
+        log.e('Invalid or no config file passed')
+
+    }
+} catch (error) {
+    log.e(error);
+}
+
